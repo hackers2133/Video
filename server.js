2bbffd11b562482bfbab26bb5b1454209f6989ef
hackers2133 <157810919+hackers2133@users.noreply.github.com>
2024-01-31 22:59:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'video', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
